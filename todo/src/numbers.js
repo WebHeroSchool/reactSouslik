b250b536cc37e333b28f09090a7eaadbcde97e3d
ChuVo <cahek764@ya.ru>
2019-05-30 18:22:26 +0300

    added module

---
+++ todo/src/numbers.js
@@ -0,0 +1,6 @@
+const count = 595;//объявили переменную
+export { count };//поставили в экспорт
+
+const number = 404;//объявили переменную
+const letter = 'D';
+export { number as n, letter as l };//поставили в экспорт
