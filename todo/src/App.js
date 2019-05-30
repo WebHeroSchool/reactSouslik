d40acf481d44b1b09a59488c45fe13992d52d4bc
ChuVo <cahek764@ya.ru>
2019-05-30 19:57:57 +0300

    added reactComponent

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 
-const myTag = ( <div>My tag</div> )
+// reactComponent
+const ItemList = () => ( <ol>
+  <li>item</li>
+  <li>item</li>
+  <li>item</li>
+</ol>);
 
-export { myTag };
+// added reactComposition
+const App = () => ( <div>
+  <h1>To do</h1>
+  <ItemList /> 
+</div> );
+
+export default App;
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { myTag } from './App';
+import App from './App';
 
-ReactDOM.render(myTag, document.getElementById('root'));
+ReactDOM.render( <App />, document.getElementById('root'));
 
