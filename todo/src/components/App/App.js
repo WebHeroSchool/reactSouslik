9caf44e35d4aefc9dc57bdb98bc41f9d72ee24eb
ChuVo <cahek764@ya.ru>
2019-06-05 14:22:14 +0300

    added class?

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -5,37 +5,38 @@ import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css'; //модуль-стили подключаются в своем компоненте
 
-const App= () => {
-
-  const items=[
-    { 
-      value : 'проснуться',
-      isDone: true
-    },
-    { 
-      value : 'побриться',
-      isDone: false
-    },
-    {
-      value : 'наступить на кота',
-      isDone: true
-    },
-    {
-      value : 'с женой поругаться',
-      isDone: false
-    }
-  ];
-
-  let isMany= false;
-
-  return (
-    <div className={ styles.todo }>
-      <h1 className={ styles.title }>Список дел</h1>
-      <InputItem />
-      <ItemList items={ items } />
-      <Footer count={ isMany } />
-    </div>
-  );
-} 
+class App extends React.Component {
+  render () {
+    const items=[
+      { 
+        value : 'проснуться',
+        isDone: true
+      },
+      { 
+        value : 'побриться',
+        isDone: false
+      },
+      {
+        value : 'наступить на кота',
+        isDone: true
+      },
+      {
+        value : 'с женой поругаться',
+        isDone: false
+      }
+    ];
+  
+    let isMany= false;
+  
+    return (
+      <div className={ styles.todo }>
+        <h1 className={ styles.title }>Список дел</h1>
+        <InputItem />
+        <ItemList items={ items } />
+        <Footer count={ isMany } />
+      </div>
+    );
+  }
+}
 
 export default App;
