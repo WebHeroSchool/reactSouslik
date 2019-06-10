e494d6dea52bf1c55d104aa42151562502cebe47
ChuVo <cahek764@ya.ru>
2019-06-10 09:38:04 +0300

    created event headler

--- todo/src/components/Item/Item.js
+++ todo/src/components/Item/Item.js
@@ -1,14 +1,22 @@
 import classnames from 'classnames';
 import React from 'react';
 import styles from './Item.module.css';
+import Checkbox from '@material-ui/core/Checkbox';
 
-const Item = ({ value, isDone }) => (
-<span className={
-    classnames({
-        [styles.item]: true,
-        [styles.done]: isDone
-	})
-}>{value}</span>
+const Item = ({ value, isDone, onClickDone }) => (
+    <div>
+        <Checkbox
+            checked={isDone}
+            tabIndex={-1}
+            onClick={() => onClickDone(isDone)}
+        />
+        <span className={
+            classnames({
+                [styles.item]: true,
+                [styles.done]: isDone
+            })
+        }>{value}</span>
+    </div>
 );
 
 export default Item;
