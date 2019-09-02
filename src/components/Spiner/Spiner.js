ad495241537b0f218ee946ef28220e143c570d6c
CrazySouslik <45027339+ChuVo@users.noreply.github.com>
2019-09-02 21:24:50 +0300

    Revert "Dev"

--- src/components/Spiner/Spiner.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './Spiner.module.css';
-
-const Spiner = () => (<img src='../images/Spiner.gif' alt="Don`t worry it`s OK!" className={styles.spiner} />);
-
-export default Spiner;
